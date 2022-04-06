bug in symbolic computation

pyrat/dynamic_system/continuous_system/nonlinear_system.py
@@ -1,12 +1,13 @@
 from __future__ import annotations
 
+import inspect
 from dataclasses import dataclass
 
 import numpy as np
 from scipy.special import factorial
-from sympy import lambdify
+from sympy import lambdify, hessian
 
-from pyrat.geometry import Geometry, VectorZonotope, Interval
+from pyrat.geometry import Geometry, VectorZonotope, Interval, cvt2
 from pyrat.misc import Reachable, Simulation
 from pyrat.model import Model
 from .continuous_system import ContSys, Option, RunTime
@@ -23,6 +24,7 @@ class NonLinSys:
         u_trans: np.ndarray = None
         zonotope_order: int = 50
         reduction_method: str = "girard"
+        tensor_order = 2
 
         def validate(self) -> bool:
             #  TODO
@@ -58,6 +60,12 @@ class NonLinSys:
             if self._jx is None:
                 self._jx = self._model.f.jacobian(self._model.vars[0])
                 self._ju = self._model.f.jacobian(self._model.vars[1])
+                self._hx = [
+                    hessian(expr, self._model.vars[0]) for expr in self._model.f
+                ]
+                self._hu = [
+                    hessian(expr, self._model.vars[1]) for expr in self._model.f
+                ]
 
         def _evaluate(self, x, u, mod: str = "numpy"):
             f = lambdify(self._model.vars, self._model.f, mod)
@@ -67,6 +75,31 @@ class NonLinSys:
             fx = lambdify(self._model.vars, self._jx, "numpy")
             fu = lambdify(self._model.vars, self._ju, "numpy")
             return fx(x, u), fu(x, u)
+
+        def _hessian(self, x, u):
+            ops = Interval.ops()
+            fx = []
+            for hx in self._hx:
+                fxi = []
+                for hxi in hx:
+                    print(hxi)
+                    fxi.append(lambdify(self._model.vars, hxi, ops))
+                fx.append(fxi)
+
+            fu = []
+            for hu in self._hu:
+                fui = []
+                for hui in hu:
+                    fui.append(lambdify(self._model.vars, hui, ops))
+                fu.append(fui)
+
+            for fi in fx:
+                for f in fi:
+                    print(str(f))
+                    print(f(x, u))
+                    exit(False)
+
+            raise NotImplementedError
 
         def _linearize(
             self, op: NonLinSys.Option, r: Geometry
@@ -120,7 +153,28 @@ class NonLinSys:
             :return:
             """
             # compute interval of reachable set
-            ihx = Interval(r)
+            ihx = cvt2(r, "int")
+            # compute intervals of total reachable set
+            total_int_x = ihx + self._run_time.lin_err_p["x"]
+
+            # compute intervals of input
+            ihu = cvt2(op.u, "int")
+            # translate intervals by linearization point
+            total_int_u = ihu + self._run_time.lin_err_p["u"]
+
+            if op.tensor_order == 2:
+                # assign correct hessian (using interval arithmetic)
+                # TODO
+
+                # obtain maximum absolute values within ihx, ihu
+                dx = np.maximum(abs(ihx.inf), abs(ihx.sup))
+                du = np.maximum(abs(ihu.inf), abs(ihu.sup))
+
+                # evaluate the hessian matrix with the selected range-bounding technique
+                h = self._hessian(total_int_x, total_int_u)
+
+                raise NotImplementedError
+
             raise NotImplementedError
 
         def _lin_reach(self, r_init: Reachable.Element, op: NonLinSys.Option):
@@ -175,7 +229,7 @@ class NonLinSys:
             self._run_time.cur_t = op.t_start
             # init reachable result
             r = Reachable.Result()
-            r.init(op.steps, op.steps)
+            # r.init(op.steps, op.steps)
             # init reachable set computation
             r_next, op = self.reach_init(op.r_init, op)
             # TODO

pyrat/geometry/interval.py
@@ -8,23 +8,13 @@ from .geometry import Geometry
 
 
 class Interval(Geometry):
-    def __init__(self, data):
-        self.__init(data)
-
-    def __init(self, data):
-        if isinstance(data, np.ndarray):
-            return self.__init_from_numpy_array(data)
-        # elif isinstance(data, VectorZonotope):
-        #     print("DO SOMETHING")
-        #     raise NotImplementedError
-
-    def __init_from_numpy_array(self, data: np.ndarray):
-        assert data.shape[0] == 2 and data.ndim >= 2
-        assert np.all(data[0] <= data[1])
-        self._bd = data
+    def __init__(self, bd: np.ndarray):
+        assert bd.shape[0] == 2 and bd.ndim >= 2
+        assert np.all(bd[0] <= bd[1])
+        self._bd = bd
         self._is_empty = False
 
-        # =============================================== property
+    # =============================================== property
 
     @property
     def bd(self) -> np.ndarray:
@@ -54,7 +44,49 @@ class Interval(Geometry):
     def center(self) -> np.ndarray:
         return self._bd.sum(axis=0) * 0.5
 
+    # =============================================== class method
+    @classmethod
+    def ops(cls):
+        return {
+            "__add__": cls.__add__,
+            "__radd__": cls.__radd__,
+            "__iadd__": cls.__iadd__,
+            "__sub__": cls.__sub__,
+            "__rsub__": cls.__rsub__,
+            "__isub__": cls.__isub__,
+            "__mul__": cls.__mul__,
+            "__matmul__": cls.__matmul__,
+            "__truediv__": cls.__truediv__,
+            "__rtruediv__": cls.__rtruediv__,
+            "__pow__": cls.__pow__,
+            "__getitem__": cls.__getitem__,
+        }
+
     # =============================================== operator
+    def __getitem__(self, item):
+        assert isinstance(item, int) and item >= 0
+        return self._bd[:, item]
+
+    def __truediv__(self, other):
+        if isinstance(other, numbers.Real):
+            return self * (1 / other)
+        else:
+            raise NotImplementedError
+
+    def __rtruediv__(self, other):
+        if isinstance(other, numbers.Real):
+            bd = other / self._bd
+            return Interval(np.flip(bd, axis=0))
+        else:
+            raise NotImplementedError
+
+    def __pow__(self, power, modulo=None):
+        if isinstance(power, numbers.Real):
+            print("==========================")
+            return Interval(self.bd**power)
+        else:
+            raise NotImplementedError
+
     def __contains__(self, item):
         raise NotImplementedError
 
@@ -93,7 +125,7 @@ class Interval(Geometry):
         return self * -1
 
     def __str__(self):
-        return str(self._bd[0]) + " " + str(self._bd[1])
+        return str(self._bd[0]) + " \n" + str(self._bd[1])
 
     def __matmul__(self, other):
         raise NotImplementedError(

test/util/test_sympy.py
@@ -135,10 +135,27 @@ def test_sympy_using_custom_interval_arithmetic():
         def __matmul__(self, other):
             raise NotImplementedError
 
+        def __pow__(self, power, modulo=None):
+            print("+++++++++++++++++++++")
+            raise NotImplementedError
+
     x, y = symbols(("x", "y"))
-    expr = x + x * y * 2
+    expr = x + y**2
     f = lambdify((x, y), expr, dict(inspect.getmembers(Interval)))
 
     temp = f(Interval(-1, 1), Interval(2, 3))
     # temp = Interval(-1, 1) * 2
     print(temp)
+
+
+def test_interval():
+    from pyrat.geometry import Interval
+
+    v = Interval(np.array([[1.79294, 0], [2.21276, 0]], dtype=float).reshape((2, -1)))
+    print(v)
+    print(v[0])
+    print(v[1])
+    x = symbols("x:2")
+    eqr = Matrix([0.0166104259427626 / x[0] ** (3 / 2), x[1]])
+    f = lambdify(x, eqr, Interval.ops())
+    print(f(v))